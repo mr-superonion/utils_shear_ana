1c6cdd14863fecee00d4ff5bf250477944bba438
Xiangchong Li <xiangchong.li@ipmu.jp>
2022-12-12 13:00:03 +0900

    add all to subbit_merge_mock_job

--- bin/submit_merge_mocks_job.sh
+++ bin/submit_merge_mocks_job.sh
@@ -5,4 +5,5 @@ xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field VVDS --d
 xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field HECTOMAP --datname $1 --mpi && sleep 0.5
 xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field GAMA09H --datname $1 --mpi && sleep 0.5
 xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field WIDE12H --datname $1 --mpi && sleep 0.5
-xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field GAMA15H --datname $1 --mpi
+xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field GAMA15H --datname $1 --mpi && sleep 0.5
+xsubMini mpirun -n 28 merge_2pcf_mock.py --minId 0 --maxId 1404 --field all --datname $1 --mpi
